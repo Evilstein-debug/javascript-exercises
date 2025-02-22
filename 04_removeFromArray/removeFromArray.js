f5943e0f4f55ce49eb6100cedbd4c2a0f3c5ae22
Evilstein-debug <tejaspathak2005@gmail.com>
2025-02-22 14:56:33 +0530

    04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...args) {
+    let test = 1
+    for(let i=0; i<arr.length; i++){
+        for(let j=0; j<args.length; j++){
+            if(arr[i] == args[j] && (typeof(arr[i]) == typeof(args[j]))){
+                arr.splice(i,1)
+                i--
+                break
+            }
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+// let arr = [0,1,2]
+// let n = 0
