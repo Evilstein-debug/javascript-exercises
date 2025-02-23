3f12670607b5279e89b532c1f83aa0805af56bde
Evilstein-debug <tejaspathak2005@gmail.com>
2025-02-23 14:30:00 +0530

    07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(T1) {
+  let C = (T1-32)*(5/9)
+  let roundedC = parseFloat(C.toFixed(1)) 
+  return roundedC
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(T2) {
+  let F = (T2*(9/5)+32)
+  let roundedF = parseFloat(F.toFixed(1))
+  return roundedF
 };
 
 // Do not edit below this line
