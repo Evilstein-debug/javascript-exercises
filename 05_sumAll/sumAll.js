e56869b3a22a6faff4076614d63a4e692598eed4
Evilstein-debug <tejaspathak2005@gmail.com>
2025-02-23 13:09:54 +0530

    05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(num1 > 0 && num2 > 0 && (typeof(num1) === typeof(num2)) && Number.isInteger(num1) && Number.isInteger(num2)){
+        if(num1>num2){
+            let sum2 = num1
+            for(let i=num1-1; i>=num2; i--){
+                sum2 += i
+            }
+            return sum2
+        }
+        else{
+            let sum = num1
+            for(let i=num1+1; i<=num2; i++){
+                sum += i
+            }
+            return sum
+        }
+    }
+    else return 'ERROR'
+
 
 };
 
